cecf0f82e66d2adc99afaa52d5869491f820d96e
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2022-01-01 22:30:30 +0100

    try heroku

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -1,6 +1,6 @@
 export const HOMEPAGE = "/";
-export const SIGNUPPAGE = "/auth/signup";
-export const LOGINPAGE = "/auth/login";
+export const SIGNUPPAGE = "/signup";
+export const LOGINPAGE = "/login";
 export const PROTECTEDPAGE = "/protected";
 export const HOMEPAGELOGIN = "/home";
 export const GAMERED = "/red/:name";
